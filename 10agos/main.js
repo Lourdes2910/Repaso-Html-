5e3af6e9cf280f26cfa3d795bb3a2ee63d054071
barbara lagos corrales <barbara.lagoscorrales@gmail.com>
2022-09-27 13:22:54 -0300

    26sep-ciclos

---
+++ 10agos/main.js
@@ -0,0 +1,8 @@
+const cambiarcolor = () => {
+    const color =document.getElementById ('bgcolor').value
+    document.bgColor = color;
+}
+const cambiarcolordesdeselect = () => {
+    const color = document.getElementById('selectcolor').value
+    document.bgColor = color
+}
---
+++ 26 sep-ciclos/main.js
@@ -0,0 +1,23 @@
+let number = 1
+do {
+        console.log(number)
+        number=number+1 // number + +
+
+}while(number<=10)
+
+let number2 = 10
+do{
+        console.log(n)
+        n=n -1
+
+}while(n>=1)
+
+const name="barbara"
+let n= name.lenght -1
+do{
+    console.log(name[n])
+    n=n-1
+}while(n>=0)
+
+
+
