2609a472556f8b94a1c647d4659d8f877a89a453
barbara lagos corrales <barbara.lagoscorrales@gmail.com>
2022-09-28 12:30:40 -0300

    ciclos,for28sept

---
+++ 28sept-ciclos/main.js
@@ -0,0 +1,48 @@
+const notas = [1.3,2.1,3.4,7.0,6.2,5.4]
+let suma =0
+let aux =0
+
+while(aux<notas.length){
+    
+    
+
+       suma += notas[aux]
+        aux++
+   
+    console.log("el total es:",suma)
+    
+}
+        const promedio= suma/notas.length
+        console.log(promedio)
+
+
+        //ciclos de for;condicion; inicializacion,actualizacion){ 
+            //} //index=index+1 suma=suma+grade[index]
+      const grades =[1.3, 2.1, 3.4, 7.0, 6.2, 5.4]
+      let sum = 0
+
+        for(contador= 0; contador< grades.length; contador++){ 
+                suma +=grades[contador]
+                console.log("acumulado:",suma)
+            
+        }
+        console.log("el total es", contador)
+
+        const numbers = [2,5,7,12,15,19]
+    
+
+let index = <10
+let suma2 = >10
+do{
+
+    sum += numbers[index]
+    console.log(suma2)
+    index++
+}while (index < numbers.length)
+console.log("el total es:",suma2)
+
+    
+
+
+    
+
