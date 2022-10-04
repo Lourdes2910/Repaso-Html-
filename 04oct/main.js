d2ab6b911a14d22fa7d9defde1f199a05618bec1
barbara lagos corrales <barbara.lagoscorrales@gmail.com>
2022-10-04 14:45:33 -0300

    true-false4oct

---
+++ 04oct/main.js
@@ -0,0 +1,22 @@
+const isAdult=(age) => { //evaluacion la condicionde edad para verdadero o falso
+    if (age >= 18) {
+        return true
+    } else {
+        return false
+    }
+}
+    const logmessage =(age)=>{   //aqui se decreta el mensaje
+        if (isAdult (age)){
+            console.log("es mayor de edad")
+        } else {
+
+        console.log ("es menor de edad")
+           }
+        }
+    const transformage = (age) => { //transfromamos el mensaje de edad a si el mayor
+        return Number(age)
+    }
+
+    const userAge= prompt ("indicarnos tu edad")
+
+    logmessage (transformage (userAge)
